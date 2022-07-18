a0ab47c68e8d36e7fca178274c76ca8ad5d4652b
Berix <berix4242@gmail.com>
2022-07-18 10:19:45 +0200

    Create new static folder for each frontend-build
    Content of webapps static folder moved to frontend.

--- after_build.py
+++ after_build.py
@@ -6,58 +6,43 @@ Moves files etc. to the correct directories and gzips js.
 
 import gzip
 import os
-import json
 import shutil
 from distutils.dir_util import copy_tree
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
 build_folder = os.path.join(root_dir, "flandria-frontend", "build")
-
-with open(os.path.join(build_folder, "asset-manifest.json"), "r") as fp:
-    manifest = json.load(fp)
-
 templates_folder = os.path.join(root_dir, "webapp", "templates")
 static_folder = os.path.join(root_dir, "webapp", "static")
 
-# Copy html to templates folder
-shutil.copyfile(os.path.join(build_folder, "index.html"),
-                os.path.join(templates_folder, "index.html"))
 
-# Copy favicon
-shutil.copyfile(os.path.join(build_folder, "favicon.ico"),
-                os.path.join(static_folder, "favicon.ico"))
+def clean_than_copy_path(source_path, target_path):
+    shutil.rmtree(target_path, ignore_errors=True)
+    copy_tree(source_path, target_path)
 
-# Copy assets
-shutil.rmtree(os.path.join(static_folder, "assets"))
-copy_tree(os.path.join(build_folder, "assets"),
-          os.path.join(static_folder, "assets"))
 
-# Copy css
-shutil.rmtree(os.path.join(static_folder, "css"))
-copy_tree(os.path.join(build_folder, "static", "css"),
-          os.path.join(static_folder, "css"))
+# Create folders
+if not os.path.exists(templates_folder):
+    os.mkdir(templates_folder)
+if not os.path.exists(static_folder):
+    os.mkdir(static_folder)
 
-# Create js folder in static
-shutil.rmtree(os.path.join(static_folder, "js"))
-if not os.path.exists(os.path.join(static_folder, "js")):
-    os.mkdir(os.path.join(static_folder, "js"))
+# Copy relevant files from build to webapp folder
+shutil.copyfile(os.path.join(build_folder, "index.html"), os.path.join(templates_folder, "index.html"))
+shutil.copyfile(os.path.join(build_folder, "favicon.ico"), os.path.join(static_folder, "favicon.ico"))
+clean_than_copy_path(os.path.join(build_folder, "assets"), os.path.join(static_folder, "assets"))
+clean_than_copy_path(os.path.join(build_folder, "files"), os.path.join(static_folder, "files"))
+clean_than_copy_path(os.path.join(build_folder, "static", "css"), os.path.join(static_folder, "css"))
 
+shutil.rmtree(os.path.join(static_folder, "js"), ignore_errors=True)
+os.mkdir(os.path.join(static_folder, "js"))
+
+# Only copy specific files from js-folder
 for fname in os.listdir(os.path.join(build_folder, "static", "js")):
     if not (fname.endswith("js") or fname.endswith("map")):
         continue
 
-    # Copy normal js file
-    shutil.copyfile(
-        os.path.join(build_folder, "static", "js", fname),
-        os.path.join(static_folder, "js", fname))
+    shutil.copy(os.path.join(build_folder, "static", "js", fname), os.path.join(static_folder, "js", fname))
 
-    with open(os.path.join(
-        build_folder, "static", "js", fname), "rb"
-    ) as fp:
-
-        # Write original content to gzipped file
-        gz_main_js = gzip.GzipFile(
-            os.path.join(static_folder, "js", f"{fname}.gz"),
-            "w")
-
-        gz_main_js.write(fp.read())
+    # Write original content to gzipped file
+    with open(os.path.join(build_folder, "static", "js", fname), "rb") as fp:
+        gzip.GzipFile(os.path.join(static_folder, "js", f"{fname}.gz"), "w").write(fp.read())
